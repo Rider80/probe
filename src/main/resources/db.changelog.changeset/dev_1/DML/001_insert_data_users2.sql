3a67f67b856d88ba74f687b3551f2fbc69031491
Alexander <alexmckey@yandex.ru>
2025-06-16 09:35:48 +0300

    first

---
+++ src/main/resources/db.changelog.changeset/dev_1/DML/001_insert_data_users2.sql
@@ -0,0 +1,2 @@
+INSERT INTO users2 (full_name, email)
+values ('Aliaksandr Makeyenka', 'alexmckey80@gmail.com');
---
+++ src/main/resources/db.changelog.changeset/dev_1/DML/002_insert_data_tasks2.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks2 (performer_id, name, start_time, deadline_time, status, task_type)
+values (1, 'Aliaksandr task', '16-06-2025',
+        '17-06-2025', 'open', 'fishing');
