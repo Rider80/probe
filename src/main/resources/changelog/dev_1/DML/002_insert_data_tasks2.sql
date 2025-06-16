f60fe361b67620c1d159dec3b444c4791c405038
Alexander <alexmckey@yandex.ru>
2025-06-16 11:02:11 +0300

    second

--- src/main/resources/changelog/dev_1/DML/002_insert_data_tasks2.sql
+++ src/main/resources/changelog/dev_1/DML/002_insert_data_tasks2.sql
@@ -1,3 +1,3 @@
 INSERT INTO tasks2 (performer_id, name, start_time, deadline_time, status, task_type)
-values (1, 'Aliaksandr task', '16-06-2025',
-        '17-06-2025', 'open', 'fishing');
+values (1, 'Aliaksandr task2', '17-06-2025',
+        '18-06-2025', 'open', 'meeting');
